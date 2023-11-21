c71c12d5259cfee3255e12f8586ef5d12a00d287
edbert2397 <edberthalim29@gmail.com>
2023-11-22 00:24:43 +0700

    connect to deployment

--- lib/screens/inventory_form.dart
+++ lib/screens/inventory_form.dart
@@ -125,7 +125,7 @@ class _InventoryFormPageState extends State<InventoryFormPage> {
                           // Kirim ke Django dan tunggu respons
                           // TODO: Ganti URL dan jangan lupa tambahkan trailing slash (/) di akhir URL!
                           final response = await request.postJson(
-                          "http://127.0.0.1:8000//create-flutter/",
+                          "https://edbert-halim-tugas.pbp.cs.ui.ac.id/create-flutter/",
                           jsonEncode(<String, String>{
                               'name': _name,
                               'amount': _amount.toString(),
--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -41,7 +41,7 @@ class _RegisterPageState extends State<RegisterPage> {
                 String password = _passwordController.text;
                 try {
                   final response = await http.post(
-                    Uri.parse("http://127.0.0.1:8000/auth/register/"),
+                    Uri.parse("https://edbert-halim-tugas.pbp.cs.ui.ac.id/auth/register/"),
                     body: {
                       'username': username,
                       'password': password,
